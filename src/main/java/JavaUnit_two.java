14fb7afb24e4f4abea4ed83588dc07b9d74c90ab
DURGASEKAR123 <durga0706@gmail.com>
2021-07-21 10:49:47 +0530

    DURGA TASK

---
+++ src/main/java/JavaUnit_two.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JavaUnit_two 
+{ 
+	@BeforeClass
+	public static void beforeclas() 
+	{
+		System.out.println("before class");
+	}
+	@AfterClass
+	public static void afterclass()
+	{
+		System.out.println("after class");
+	}
+	@Test
+	public void after() 
+	{
+		System.out.println("after");
+		Date date = new Date();
+		System.out.println(date);
+	}
+	@Before
+	public void before() 
+	{
+		System.out.println("before");
+		Date date = new Date();
+		System.out.println(date);
+	}
+	@Test
+	public void testmethod() 
+	{
+     System.out.println("test method 1");
+     System.out.println("test 2");
+	}
+}
